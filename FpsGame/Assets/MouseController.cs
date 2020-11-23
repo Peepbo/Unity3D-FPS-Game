1289a33d927ca3fd6b589b3682f5a175bffcc89f
채수현 <tngus10778@gmail.com>
2020-11-23 17:15:03 +0900

    playerMovement

---
+++ FpsGame/Assets/MouseController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    public float Vcam, Hcam;
+
+    //GameObject head;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //head = transform.Find("Head").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //카메라에서 가져오기위함
+        Hcam += Input.GetAxis("Mouse X");
+        Vcam -= Input.GetAxis("Mouse Y");
+
+        if (Vcam > 70) Vcam = 70;
+        else if (Vcam < -30) Vcam = -30;
+
+        //MouseX = Input.GetAxit("Mouse X");
+
+        //transform.Rotate(Vector3.up * 1f * Hcam);
+        //transform.Rotate(Vector3.left * 1f * Vcam);
+        transform.rotation = Quaternion.Euler(1f * Vcam, 1f * Hcam, 0f);
+        //transform.Rotate(new Vector3(1f * Hcam, 1f * Vcam, 0f));
+        //transform.rotation.z = 0f;
+    }
+}
---
+++ FpsGame/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    CharacterController cc;
+
+    Transform head;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Quaternion dir = transform.rotation;
+        if (Input.GetKey(KeyCode.W))
+        {
+            cc.Move(Vector3.forward * speed * Time.deltaTime);
+        }
+    }
+}
